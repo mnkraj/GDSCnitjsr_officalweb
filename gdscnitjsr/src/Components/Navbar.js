af296232d89c506a65cc1ea1caddd7506b870c52
Mayank Raj <mayankraj7100@gmail.com>
2024-02-14 18:19:56 +0530

    fixed navbar  responisve isuue

--- gdscnitjsr/src/Components/Navbar.js
+++ gdscnitjsr/src/Components/Navbar.js
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.jpg";
+
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-2">
@@ -9,21 +16,16 @@ const Navbar = () => {
             href="https://gdsc.community.dev/national-institute-of-technology-nit-jamshedpur/"
             className="flex items-center space-x-3 rtl:space-x-reverse"
           >
-            <img
-              src={logo}
-              className="h-6"
-              alt="Flowbite Logo"
-            />
+            <img src={logo} className="h-6" alt="Flowbite Logo" />
             <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
               GDSC NITJSR
             </span>
           </a>
           <button
-            data-collapse-toggle="navbar-solid-bg"
+            onClick={toggleMenu}
             type="button"
             className="inline-flex items-center p-2 w-8 h-8 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-            aria-controls="navbar-solid-bg"
-            aria-expanded="false"
+            aria-expanded={isOpen ? "true" : "false"}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -43,7 +45,11 @@ const Navbar = () => {
             </svg>
           </button>
         </div>
-        <div className="hidden w-full md:block md:w-auto" id="navbar-solid-bg">
+        <div
+          className={`${
+            isOpen ? "block" : "hidden"
+          } w-full md:flex md:w-auto`}
+        >
           <ul className="flex flex-col font-medium mt-2 rounded-lg bg-gray-50 md:space-x-4 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700">
             <li>
               <a
